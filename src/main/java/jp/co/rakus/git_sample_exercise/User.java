d023d9750a463efef4d07ae4694926f5520f3af2
shun.nakano <shun.nakano@rakus.co.jp>
2017-08-08 15:25:20 +0900

    メールアドレスと誕生日の項目を追加しました

--- src/main/java/jp/co/rakus/git_sample_exercise/User.java
+++ src/main/java/jp/co/rakus/git_sample_exercise/User.java
@@ -1,5 +1,7 @@
 package jp.co.rakus.git_sample_exercise;
 
+import java.sql.Date;
+
 /**
  * ユーザーを表すドメイン.
  * @author shun.nakano
@@ -10,6 +12,23 @@ public class User {
 	private Integer id;
 	/** 名前 */
 	private String name;
+	/** メールアドレス */
+	private String email;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	/** 誕生日(年月日) */
+	private Date birthday;
 	public Integer getId() {
 		return id;
 	}
